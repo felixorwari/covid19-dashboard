d681e0e0cccd52073cc201227182fc51d55c7a3b
Felix Orwari <felix.orwari@gmail.com>
2023-11-09 11:53:56 +0300

    Created filter buttons

---
+++ src/components/FilterButtons.vue
@@ -0,0 +1,20 @@
+<script></script>
+
+<template>
+    <!-- Filter Buttons -->
+    <div class="inline-flex text-xs rounded-md shadow-sm" role="group">
+      <button type="button" class="px-4 py-2 font-medium border rounded-l-lg border-gray-400/50">
+        Today
+      </button>
+      <button type="button" class="px-4 py-2 font-medium border border-gray-400/50">
+        This Week
+      </button>
+      <button type="button" class="px-4 py-2 font-medium border border-gray-400/50">
+        This Month
+      </button>
+      <button type="button" class="px-4 py-2 font-medium border rounded-r-lg border-gray-400/50">
+        This Year
+      </button>
+    </div>
+    <!-- /End Filter Buttons -->
+</template>
