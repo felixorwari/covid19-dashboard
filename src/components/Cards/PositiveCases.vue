8fd4cf38a6bbed6d17a192c71976a0ac38d13d62
Felix Orwari <felix.orwari@gmail.com>
2023-11-09 14:43:58 +0300

    Created new statistics card components

---
+++ src/components/Cards/PositiveCases.vue
@@ -0,0 +1,29 @@
+<script>
+import PlusCircleIcon from '../Icons/PlusCircleIcon.vue';
+
+export default {
+  components: {
+    PlusCircleIcon
+  }
+}
+</script>
+
+<template>
+    <div class="bg-white rounded-lg shadow">
+        <div class="flex px-6 py-7">
+          <div class="flex items-center px-5 py-3 mr-5 text-white bg-[#D13F4A] rounded-md shadow-lg">
+            <plus-circle-icon></plus-circle-icon>
+          </div>
+          <div>
+            <h5>Positive Cases</h5>
+            <p class="text-4xl font-bold">2,450
+              <span class="ml-2 text-sm text-red-600"><i class="fa-solid fa-arrow-up"></i> 5.4%</span>
+            </p>
+          </div>
+        </div>
+        <div class="flex justify-between p-4 text-sm uppercase bg-gray-100 rounded-b-lg">
+          <p class="font-bold text-gray-400">Males: <span class="font-normal">1,932</span></p>
+          <p class="font-bold text-gray-400">Females: <span class="font-normal">518</span></p>
+        </div>
+    </div>
+</template>
---
+++ src/components/Cards/RegisteredLabs.vue
@@ -0,0 +1,28 @@
+<script>
+import UsersGroupIcon from '../Icons/UsersGroupIcon.vue';
+
+export default {
+  components: {
+    UsersGroupIcon
+  }
+}
+</script>
+
+<template>
+    <div class="bg-white rounded-lg shadow">
+        <div class="flex px-6 py-7">
+          <div class="flex items-center px-5 py-3 mr-5 text-white bg-[#2c7be5] rounded-md shadow-lg">
+            <users-group-icon></users-group-icon>
+          </div>
+          <div>
+            <h5 class="text-gray-500">Registered Labs</h5>
+            <p class="text-4xl font-bold">20 
+              <span class="ml-2 text-sm text-green-600"><i class="fa-solid fa-arrow-up"></i> 2</span>
+            </p>
+          </div>
+        </div>
+        <div class="p-4 text-sm bg-gray-100 rounded-b-lg">
+          <p>&nbsp;</p>
+        </div>
+    </div>
+</template>
