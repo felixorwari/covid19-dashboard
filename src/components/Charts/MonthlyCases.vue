320d4dc11867a69b158206d54599281f986c82e0
Felix Orwari <felix.orwari@gmail.com>
2023-11-10 11:25:57 +0300

    Added dynamic data fetching

--- src/components/Charts/MonthlyCases.vue
+++ src/components/Charts/MonthlyCases.vue
@@ -2,9 +2,8 @@
 export default {
     data() {
         return {
-            options: {
+            chartOptions: {
                 chart: {
-                    height: 'auto',
                     toolbar: {
                         show: false
                     }
@@ -13,38 +12,53 @@ export default {
                     enabled: false,
                 },
                 stroke: {
-                    width: 2,
+                    width: 3,
                     colors: ["#fff"]
                 },
                 xaxis: {
                     categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
                 },
-                colors: ["#D13F4A", "#D2DDEC"],
+                colors: ["#D13F4A", "#92AAEC"],
                 legend: {
                     show: false
                 },
                 plotOptions: {
                     bar: {
-                        borderRadius: 8,
-                        borderRadiusApplication: 'around',
+                        borderRadius: 9,
+                        borderRadiusApplication: 'end',
                         columnWidth: '65%'
                     }
                 },
             },
-            series: [
+            chartSeries: [],
+        }
+    },
+    async mounted() {
+        // Fetch chart data
+        try {
+            const response = await fetch('/data/monthly-covid-cases.json');
+            const data = await response.json();
+            this.processChartData(data);
+        } catch (error) {
+            console.error('Error fetching monthly covid cases data:', error);
+        }
+    },
+    methods: {
+        // Hydrate chart series data
+        processChartData(data) {
+            const positiveData = Object.values(data).map((entry) => entry.positive);
+            const negativeData = Object.values(data).map((entry) => entry.negative);
+
+            this.chartSeries = [
                 {
-                    name: 'positive',
-                    data: [
-                        520, 256, 750, 400, 100, 220, 150, 614, 648, 523, 269, 840
-                    ]
+                    name: "Positive",
+                    data: positiveData
                 },
                 {
-                    name: 'negative',
-                    data: [
-                        1161, 650, 1920, 700, 379, 384, 432, 1432, 1749, 1100, 600, 2200
-                    ]
-                },
-            ],
+                    name: "Negative",
+                    data: negativeData
+                }
+            ]
         }
     }
 }
@@ -53,8 +67,8 @@ export default {
 <template>
     <div class="bg-white rounded-lg shadow">
         <div class="px-6 py-5 border-b">
-            <h4 class="font-semibold">Covid-19 Cases</h4>
+            <h4 class="font-semibold">Covid-19 Monthly Cases</h4>
         </div>
-        <apexchart :width="620" :height="height" type="bar" :options="options" :series="series" class="flex justify-center mx-auto"></apexchart>
+        <apexchart :width="820" :height="385" type="bar" :options="chartOptions" :series="chartSeries" class="flex justify-center mx-auto"></apexchart>
     </div>
 </template>
